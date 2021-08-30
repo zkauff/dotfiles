0badc53246a2c15b75720912536731273c6b73a1
Zach Kauffman <31994349+zkauff@users.noreply.github.com>
2021-08-29 21:19:25 -0500

    Added modified polybar files

---
+++ launch.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+
+# Terminate already running bar instances
+killall -q polybar
+
+# Wait until the processes have been shut down
+while pgrep -u $UID -x polybar >/dev/null; do sleep 1; done
+
+echo "Bars launched..."
+polybar main --config=~/.config/polybar/config.ini
